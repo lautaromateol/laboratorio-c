1f7453b10d2825e4eed2e7dbf03571dccc4d9ee1
lautaromateol <lautaromateol@gmail.com>
2025-10-22 19:35:20 -0300

    update: logica de busqueda implementada

--- main.c
+++ main.c
@@ -1,36 +1,69 @@
 #include <stdio.h>
 #include "cola.h"
 #include "mapa.h"
+#include "auxiliares.h"
 
-int main() {
-    printf("=== PROBANDO LA COLA ===\n");
-    Cola* cola = crearCola();
-    
-    Posicion p1 = {1, 1};
-    Posicion p2 = {2, 2};
-    Posicion p3 = {3, 3};
-    
-    printf("Encolando posiciones...\n");
-    encolar(cola, p1);
-    encolar(cola, p2);
-    encolar(cola, p3);
-    
-    printf("Desencolando...\n");
-    while (!estaVacia(cola)) {
-        Posicion pos = desencolar(cola);
-        printf("Posicion: (%d, %d)\n", pos.x, pos.y);
-    }
-    
-    liberarCola(cola);
-    
-    // Probar el mapa
-    printf("\n=== MAPA ===\n");
+int main()
+{
     char mapa[filas][columnas];
+    Posicion padres[filas][columnas];
+    bool visitados[filas][columnas] = {false};
+
     Posicion robot = {1, 1};
     Posicion destino = {3, 3};
-    
+
+    inicializarPadres(padres);
     inicializarMapa(mapa, robot, destino);
     imprimirMapa(mapa);
-    
+
+    Cola* cola = crearCola();
+    encolar(cola, robot);
+    visitados[robot.x][robot.y] = true;
+
+    while (!estaVacia(cola))
+    {
+        Posicion actual = desencolar(cola);
+
+        if (actual.x == destino.x && actual.y == destino.y)
+        {
+            printf("Destino encontrado.\n");
+            break;
+        }
+
+        int vx[] = {-1, 1, 0, 0};
+        int vy[] = {0, 0, -1, 1};
+
+        for (int i = 0; i < 4; i++)
+        {
+            Posicion vecino = {actual.x + vx[i], actual.y + vy[i]};
+            if (esValido(vecino, mapa, visitados))
+            {
+                padres[vecino.x][vecino.y] = actual;
+                visitados[vecino.x][vecino.y] = true;
+                encolar(cola, vecino);
+            }
+        }
+    };
+
+    Posicion camino[filas * columnas];
+    int pasos = 0;
+    camino[pasos++] = destino;
+    int i = destino.x;
+    int j = destino.y;
+    while (padres[i][j].x != -1 && padres[i][j].y != -1)
+    {
+        camino[pasos++] = padres[i][j];
+        i = padres[i][j].x;
+        j = padres[i][j].y;
+    }
+
+    for (int i = pasos - 1; i >= 0; i--)
+    {
+        printf("(%d, %d)\n", camino[i].x, camino[i].y);
+    }
+
+    mostrarCamino(mapa, pasos, camino);
+
+    liberarCola(cola);
     return 0;
 }
