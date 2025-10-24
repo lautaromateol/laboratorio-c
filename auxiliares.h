226963e23083a83b8053d97adaa9ad84f5ac163f
lautaromateol <lautaromateol@gmail.com>
2025-10-24 16:33:06 -0300

    add: funcion para leer coordenadas

--- auxiliares.h
+++ auxiliares.h
@@ -3,6 +3,7 @@
 #include "cola.h"
 #include "mapa.h"
 
+int leerCoordenadas(const char* mensaje, int min, int max);
 bool esValido(Posicion vecino, char mapa[filas][columnas], bool visitados[filas][columnas]);
 void inicializarPadres(Posicion padres[filas][columnas]);
 void mostrarCamino(char mapa[filas][columnas], int pasos, Posicion camino[]);
