3147b69f64c145115cb58d5176d2921e9905645d
lautaromateol <lautaromateol@gmail.com>
2025-10-24 16:32:51 -0300

    add: funcion para leer coordenadas

--- auxiliares.c
+++ auxiliares.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include "auxiliares.h"
 
+int leerCoordenadas(const char *mensaje, int min, int max)
+{
+  int valor;
+  do
+  {
+    printf("\n%s: (%d..%d)\n", mensaje, min, max);
+    scanf("%d", &valor);
+    if (valor < min || valor > max)
+    {
+      printf("Valor invalido para la coordenada.");
+    }
+  } while (valor < min || valor > max);
+
+  return valor;
+}
+
 bool esValido(Posicion vecino, char mapa[filas][columnas], bool visitados[filas][columnas])
 {
   return (vecino.x > 0 && vecino.y > 0) && (vecino.x < filas && vecino.y < columnas) && mapa[vecino.x][vecino.y] != pared && !visitados[vecino.x][vecino.y];
@@ -32,7 +48,10 @@ void mostrarCamino(char mapa[filas][columnas], int pasos, Posicion camino[])
 
   for (int i = 1; i < pasos - 1; i++)
   {
-    mapaCamino[camino[i].x][camino[i].y] = recorrido;
+    if (mapa[camino[i].x][camino[i].y] != rob && mapa[camino[i].x][camino[i].y] != dest)
+    {
+      mapaCamino[camino[i].x][camino[i].y] = recorrido;
+    }
   }
 
   for (int i = 0; i < filas; i++)
