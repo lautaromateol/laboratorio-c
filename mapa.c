169f04b199c218dff3d464a4be4b9c5fc0850486
lautaromateol <lautaromateol@gmail.com>
2025-10-24 16:33:59 -0300

    update: obstaculos aleatorios añadidos a mapa

--- mapa.c
+++ mapa.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include "mapa.h"
 
 void inicializarMapa(char mapa[filas][columnas], Posicion robot, Posicion destino)
 {
+  static int inicializado = 0;
+  if (!inicializado)
+  {
+    srand(time(NULL));
+    inicializado = 1;
+  }
+
   for (int i = 0; i < filas; i++)
   {
     for (int j = 0; j < columnas; j++)
     {
-      if (i == 0 || i == filas - 1 || j == 0 || j == columnas - 1 || (i == 2 && j == 2))
-      {
-        mapa[i][j] = pared;
-      }
-      else if (i == robot.x && j == robot.y)
+      mapa[i][j] = libre;
+    }
+  }
+
+  for (int i = 0; i < filas; i++)
+  {
+    mapa[i][0] = pared;
+    mapa[i][columnas - 1] = pared;
+  }
+
+  for (int j = 0; j < columnas; j++)
+  {
+    mapa[0][j] = pared;
+    mapa[filas - 1][j] = pared;
+  }
+
+  for (int i = 0; i < filas; i++)
+  {
+    for (int j = 0; j < columnas; j++)
+    {
+      if (i == robot.x && j == robot.y)
       {
         mapa[i][j] = rob;
       }
@@ -19,8 +44,24 @@ void inicializarMapa(char mapa[filas][columnas], Posicion robot, Posicion destin
       {
         mapa[i][j] = dest;
       }
+    }
+  }
+
+  for (int i = 1; i < filas - 1; i++)
+  {
+    for (int j = 1; j < columnas - 1; j++)
+    {
+      if ((i == robot.x && j == robot.y) || (i == destino.x && j == destino.y))
+      {
+        continue;
+      }
       else
-        mapa[i][j] = libre;
+      {
+        if (rand() % 100 < 20)
+        {
+          mapa[i][j] = pared;
+        }
+      }
     }
   }
 }
