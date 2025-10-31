660e5b7ccc51ff9f6f79949cb7b55a59cb8161e8
lautaromateol <lautaromateol@gmail.com>
2025-10-30 23:44:41 -0300

    update: declaracion de filas y columnas

--- mapa.h
+++ mapa.h
@@ -1,14 +1,14 @@
 #ifndef MAPA_H
 #define MAPA_H
 #include "cola.h"
-
-#define filas 10
-#define columnas 10
 #define pared '*'
 #define libre '.'
 #define recorrido '+'
 #define rob 'R'
 #define dest 'D'
+
+extern int filas;
+extern int columnas;
 
 void inicializarMapa(char mapa[filas][columnas], Posicion robot, Posicion destino);
 void imprimirMapa(char mapa[filas][columnas]);
